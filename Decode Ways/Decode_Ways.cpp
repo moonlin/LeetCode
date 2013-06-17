d2a068b25c69558702cba8f6f00fedbc792c2790
moonlin <fixme2013@163.com>
2013-06-18 00:49:44 +0800

    leetcode_Decode_ways
    not effective method

---
+++ Decode Ways/Decode_Ways.cpp
@@ -0,0 +1,99 @@
+/***************************************
+
+A message containing letters from A-Z is being encoded to numbers using the following mapping:
+
+'A' -> 1
+'B' -> 2
+...
+'Z' -> 26
+Given an encoded message containing digits, determine the total number of ways to decode it.
+
+For example,
+Given encoded message "12", it could be decoded as "AB" (1 2) or "L" (12).
+
+The number of ways decoding "12" is 2.
+
+***************************************/
+
+#include <vector>
+#include <string>
+#include <queue>
+#include <map>
+#include <stack>
+
+using namespace std;
+
+typedef unsigned int uint;
+ 
+// not effective method...
+class Solution {
+public:
+	int numDecodings(string& str)
+	{
+		if (str.empty()) return 0;
+
+		memoSingle.clear();
+		memoSingle.resize(str.size());
+		for (int i = 0; i < str.size(); ++i) {
+			memoSingle[i] = 0;
+		}
+
+		memoDouble.clear();
+		memoDouble.resize(str.size());
+		for (int i = 0; i < str.size(); ++i) {
+			memoDouble[i] = 0;
+		}
+
+		return this->numDecodings_impl(str, 0);
+	}
+
+	int numDecodings_impl(string& str, int pos)
+	{
+		if (pos >= str.size()) return 1;
+
+		int total = 0;
+		if (valid(str, pos)) {
+			total = memoDouble[pos]? memoDouble[pos]: memoDouble[pos] = numDecodings_impl(str, pos+2);
+		}
+
+		if (str[pos] != '0') {
+			total += memoSingle[pos]? memoSingle[pos]: memoSingle[pos] = numDecodings_impl(str, pos+1);
+		}
+
+		return total;
+	}
+
+
+	bool valid(const std::string str, int pos)
+	{
+		if (pos >= str.size()) return false;
+		if (pos == str.size()-1) return false;
+		if (str[pos] == '0') return false;
+
+		int num = 0;
+		int loop = 0;
+		while (loop < 2 && pos < str.size()) {
+			num = num * 10 + str[pos] - '0';
+			++pos;
+			++loop;
+		}
+
+		if (num > 26 || num < 1) return false;
+		return true;
+	}
+
+	std::vector<int> memoSingle;
+	std::vector<int> memoDouble;
+};
+
+int main()
+{	
+	// std::string str("10");
+	std::string str("4757562545844617494555774581341211511296816786586787755257741178599337186486723247528324612117156948");
+	Solution solve;
+	int ret = solve.numDecodings(str);
+
+	getchar();
+	return 0;
+}
+
